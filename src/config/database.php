67f68a00c0737e1afbe8bb566ba04b6b62d55258
Josue Pena <josuemorianp@gmail.com>
2023-10-03 14:11:50 -0400

    first commit

--- src/config/database.php
+++ src/config/database.php
@@ -1,14 +1,14 @@
 <?php
   
   try {
-      $hostname = "localhost";
-      $username = "root";
-      $password = "";
-      $dbname = "universidad_db";
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "universidad_db";
   
-      $dsn = "mysql:host=$hostname; dbname=$dbname";
-      $pdo = new PDO($dsn, $username, $password);
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn = "mysql:host=$hostname; dbname=$dbname";
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
   }catch (PDOException $e) {
       echo "Error: " . $e -> getMessage();
